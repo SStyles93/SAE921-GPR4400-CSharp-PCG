7b23135dd91099e5e60925e9825935597de82ba2
BenjaminGonvers <gonversbenjamin@gmail.com>
2022-05-20 10:08:22 +0200

    adding a list in mapNode that have all the link to the other adjacent room.

--- PCG/Assets/03_Scripts/PCG_TestBen/MapNode.cs
+++ PCG/Assets/03_Scripts/PCG_TestBen/MapNode.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,19 @@ using UnityEngine.Serialization;
 
 public class MapNode : MonoBehaviour
 { 
-   //this is the size of the room, 
-   public Vector2Int sizeRoom;
-   
-   
-   
+   //this is the size of the room,nothing less or more.
+   public Vector2Int sizeRoom = Vector2Int.zero;
+
+   public List<MapNodeLink> _linkToOtherNode;
+
+   MapNode()
+   {
+      _linkToOtherNode = new List<MapNodeLink>();
+   }
+
    public MapNode(Transform newTransform,Vector2Int size)
    {
-      
       sizeRoom = size;
-
    }
 
    
