9f1d1e5daad075d0f3590b03b51033766bf34b8e
SStyles93 <samuel.styles93@gmail.com>
2022-06-02 15:13:01 +0200

    U2D Library Removed - Compile error

--- PCG/Assets/03_Scripts/PCG/MapNode.cs
+++ PCG/Assets/03_Scripts/PCG/MapNode.cs
@@ -1,9 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.U2D.Animation;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class MapNode : MonoBehaviour
 {
